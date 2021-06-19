95ab182bc33bcad44c8f78fb3ad0861c80b662ac
ryan.perrina <ryan.perrina@studio.unibo.it>
2021-06-19 11:26:01 +0200

    ho tolto le parentesi e print

--- src/chat_client.py
+++ src/chat_client.py
@@ -13,17 +13,17 @@ def receive():
             #quando viene chiamata la funzione receive, si mette in ascolto dei messaggi che
             #arrivano sul socket
             my_msg = client_socket.recv(BUFSIZ).decode("utf8")
-            if (my_msg == "INIZIO GIOCO!"):
+            if my_msg == "INIZIO GIOCO!":
                 questionText.pack()
                 answerField.pack()
                 btn_answer.pack()
                 t=random.randint(1,3)
                 gameFrame= tk.Frame(master = window)
-                if(t==1):
+                if t==1:
                     btn_A=tk.Button(master = gameFrame, text = "A", command = close)
                     btn_B=tk.Button(master = gameFrame, text = "B", command = question)
                     btn_C=tk.Button(master = gameFrame, text = "C", command = question)
-                elif(t==2):  
+                elif t==2:  
                     btn_B=tk.Button(master = gameFrame, text = "B", command = close)
                     btn_A=tk.Button(master = gameFrame, text = "A", command = question)
                     btn_C=tk.Button(master = gameFrame, text = "C", command = question)
@@ -31,7 +31,7 @@ def receive():
                      btn_C=tk.Button(master = gameFrame, text = "C", command = close)
                      btn_B=tk.Button(master = gameFrame, text = "B", command = question)
                      btn_A=tk.Button(master = gameFrame, text = "A", command = question)
-                print(t)
+              
                 btn_A.pack(side=tk.LEFT)
                 btn_B.pack(side=tk.LEFT)           
                 btn_C.pack(side=tk.LEFT)
