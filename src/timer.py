76efa1f94f1e482cdd871d57fc24cbc7b7c5ffc4
HDavi <davide.merli5@studio.unibo.it>
2021-06-26 12:50:33 +0200

    Risolti errori in classe Timer (funzione converti)

--- src/timer.py
+++ src/timer.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 import time as t
 from threading import Thread
-from tkinter import Label
 
 class Timer:
     
-    def __init__(self, label):
+    def __init__(self):
         Thread.__init__(self)
         self.contatore = 120
-        self.label = label
-        self.countdown()
         
     def countdown(cls):
         while cls.contatore > 0:
             t.sleep(1)
-            cls.contatore = cls.contatore - 1;
-            cls.label.config(text = cls.converti(cls.contatore))
+            cls.contatore -= 1;
+            return cls.converti(cls.contatore)
             
     def converti(cls, numero):
         minuti = 0
-        while numero > 60:
+        while numero >= 60:
             minuti = minuti + 1
             numero = numero - 60
         secondi = numero
         #sistemare la stampa e poi forse va
-        return minuti + ':' + secondi + 's'
-            
-            
+        s = ""
+        s = s + str(minuti) + ':' + str(secondi) + 's'
+        return s
