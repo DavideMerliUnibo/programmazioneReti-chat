710add16395a07346263ca8a96da11a237f2a0bf
HDavi <davide.merli5@studio.unibo.it>
2021-07-03 15:46:17 +0200

    Miglioramento funzione countdown()

--- src/timer.py
+++ src/timer.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 import time as t
+import tkinter as tk
 from threading import Thread
 
 class Timer:
     
-    def __init__(self):
+    def __init__(self, ):
         Thread.__init__(self)
         self.counter = 120
+        self.timerLabel = tk.Label(text = self.converti(self.counter))
         
     def countdown(cls):
         while cls.counter > 0:
             t.sleep(1)
             cls.counter -= 1;
-            return cls.converti(cls.counter)
+            cls.timerLabel.config(text = cls.converti(cls.counter))
             
     def converti(cls, num):
         minutes = 0
         while num >= 60:
-            minutes = minutes + 1
-            num = num - 60
+            minutes += 1
+            num -= 60
         seconds = num
         return '{0:02d}:{1:02d}s'.format(minutes, seconds)
